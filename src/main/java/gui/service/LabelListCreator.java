40f7216966c8a96e9adb35f3704b6883b75a477f
RomaFoRmat <roman160694@mail.ru>
2021-09-16 21:34:03 +0300

    apache poi???

--- src/main/java/gui/service/LabelListCreator.java
+++ src/main/java/gui/service/LabelListCreator.java
@@ -2,14 +2,8 @@ package gui.service;
 
 import gui.model.TestLabel;
 import gui.repository.TestLabelRepository;
-import org.apache.poi.ss.usermodel.Cell;
-import org.apache.poi.ss.usermodel.CellType;
-import org.apache.poi.ss.usermodel.DataFormatter;
-import org.apache.poi.ss.usermodel.Row;
-import org.apache.poi.xssf.usermodel.XSSFCell;
-import org.apache.poi.xssf.usermodel.XSSFRow;
-import org.apache.poi.xssf.usermodel.XSSFSheet;
-import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.xssf.usermodel.*;
 import org.bouncycastle.util.test.Test;
 
 import java.awt.*;
@@ -24,34 +18,35 @@ public class LabelListCreator {
 
     public static void createExcelList() throws IOException {
 
-        // Create a file object
-        // for the path of existing Excel file
-        // Give the path of the file as parameter
-        // from where file is to be read
+
         File fileTemp = new File("src\\main\\resources\\temp\\Export.xlsx");
+//
+//        // Create a FileInputStream object
+//        // for getting the information of the file
+//        FileInputStream fis = new FileInputStream(fileTemp);
 
-        // Create a FileInputStream object
-        // for getting the information of the file
-        FileInputStream fis = new FileInputStream(fileTemp);
+      InputStream inp = new FileInputStream("src\\main\\resources\\temp\\Export.xlsx");
+            Workbook wb = WorkbookFactory.create(inp);
+            Sheet sheet = wb.getSheetAt(0);
+            Row row = sheet.getRow(4);
+            Cell cell = row.getCell(0);
+            if (cell == null)
+                cell = row.createCell(0);
+            cell.setCellValue("TEST");
+            // Write the output to a file
+            OutputStream fileOut = new FileOutputStream("src\\main\\resources\\temp\\Export.xlsx");
+                Desktop.getDesktop().edit(fileTemp);
+                wb.write(fileOut);
 
-        // Getting the workbook instance for XLSX file
-        XSSFWorkbook workbook = new XSSFWorkbook(fis);
-        XSSFSheet sheet = workbook.getSheet("1х2х0,30SHT Ульян");
-        XSSFRow row = sheet.createRow(4);
-        row.createCell(1).setCellValue("123");
-
-        FileOutputStream fos = new FileOutputStream("Export.xlsx");
-        workbook.write(fos);
-        fos.close();
-
-        // Ensure if file exist or not
-        if (fileTemp.isFile() && fileTemp.exists()) {
-            Desktop.getDesktop().edit(fileTemp);
-            System.out.println("src\\main\\resources\\temp\\Export.xlsx");
-        } else {
-            System.out.println("Export.xlsx either not exist"
-                    + " or can't open");
         }
 
+//        // Ensure if file exist or not
+//        if (fileTemp.isFile() && fileTemp.exists()) {
+//            Desktop.getDesktop().edit(fileTemp);
+//            System.out.println("src\\main\\resources\\temp\\Export.xlsx");
+//        } else {
+//            System.out.println("Export.xlsx either not exist"
+//                    + " or can't open");
+//        }
+
     }
-}
